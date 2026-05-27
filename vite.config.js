12972a6dd9eb2d6f689db34fd7056d179264d711
Shri <shrinakul410@gmail.com>
2026-05-26 21:30:42 -0400

    Added base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@reactjs/react-dev-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/HistoryProject/",
+});
